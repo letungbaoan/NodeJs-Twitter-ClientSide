65032be1bcc1184e7d7d8c5198ae709a1bbed46e
letungbaoan <baoan7604@gmail.com>
2024-02-24 17:02:23 +0700

    Add verify email page

---
+++ src/VerifyEmail.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import useQueryParams from "./useQueryParams";
+import axios from "axios";
+
+export default function VerifyEmail() {
+  const [message, setMessage] = useState();
+  const { token } = useQueryParams();
+  useEffect(() => {
+    const controller = new AbortController();
+    if (token) {
+      axios
+        .post(
+          "/users/verify-email",
+          { email_verify_token: token },
+          {
+            baseURL: import.meta.env.VITE_API_URL,
+            signal: controller.signal,
+          }
+        )
+        .then((res) => {
+          setMessage(res.data.message);
+          if (res.data.result) {
+            const { access_token, refresh_token } = res.data.result;
+            localStorage.setItem("access_token", access_token);
+            localStorage.setItem("refresh_token", refresh_token);
+          }
+        }).catch((err) => {
+            console.log(err)
+        });
+    }
+    return () => {
+      controller.abort();
+    };
+  }, [token]);
+  return <div>{message}</div>;
+}
--- src/router.jsx
+++ src/router.jsx
@@ -1,18 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
+import VerifyEmail from "./VerifyEmail";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: (
-            <Home />
-        )
-    },
-    {
-        path: "/login/oauth",
-        element: <Login />
-    }
-])
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login/oauth",
+    element: <Login />,
+  },
+  {
+    path: "/verify-email",
+    element: <VerifyEmail />,
+  },
+]);
 
-export default router
+export default router;
