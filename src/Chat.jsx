dce025c096e1efc4ba61b34d389e70bdc500b6d8
letungbaoan <baoan7604@gmail.com>
2024-02-28 23:21:34 +0700

    Send message both way

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,44 +1,96 @@
 import { useEffect, useState } from "react";
 import socket from "./socket";
+import axios from "axios";
 
 const profile = JSON.parse(localStorage.getItem("profile"));
+const usernames = [
+  {
+    name: "user1",
+    value: "user65c123c0c78e1e9e55a0280d",
+  },
+  {
+    name: "user2",
+    value: "user65dc7e771844b8472accc7ec",
+  }
+];
 export default function Chat() {
   const [value, setValue] = useState("");
   const [messages, setMessages] = useState([]);
+  const [receiver, setReceiver] = useState("");
+  const getProfile = (username) => {
+    axios
+      .get(`/users/${username}`, {
+        baseURL: import.meta.env.VITE_API_URL,
+      })
+      .then((res) => {
+        setReceiver(res.data.result._id);
+        alert(`Now you can chat with ${res.data.result.name}`)
+      });
+  };
   useEffect(() => {
     socket.auth = {
       _id: profile._id,
     };
     socket.connect();
     socket.on("receive private message", (data) => {
-      const content = data.content
-      setMessages((messages) => [...messages, content]);
+      const content = data.content;
+      setMessages((messages) => [
+        ...messages,
+        {
+          content: content,
+          isSender: false,
+        },
+      ]);
     });
     return () => {
       socket.disconnect();
     };
   }, []);
 
-  const handleSubmit = (e) => {
+  const send = (e) => {
     e.preventDefault();
     setValue("");
     socket.emit("private message", {
       content: value,
-      to: "65dc7e771844b8472accc7ec",
+      to: receiver,
     });
+    setMessages((messages) => [
+      ...messages,
+      {
+        content: value,
+        isSender: true,
+      },
+    ]);
   };
 
   return (
     <div>
       <h1>Chat</h1>
       <div>
-        {messages.map((message, index) => (
-          <div key={index}>
-            <div>{message}</div>
+        {usernames.map((username) => (
+          <div key={username.name}>
+            <button onClick={() => getProfile(username.value)}>
+              {username.name}
+            </button>
           </div>
         ))}
       </div>
-      <form onSubmit={handleSubmit}>
+      <div className="chat">
+        {messages.map((message, index) => (
+          <div key={index}>
+            <div className="message-container">
+              <div
+                className={
+                  "message " + (message.isSender ? "message-right message" : "")
+                }
+              >
+                {message.content}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={send}>
         <input
           type="text"
           onChange={(e) => setValue(e.target.value)}
